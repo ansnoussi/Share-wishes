a1015e2d8c469370d1015b57fffecfb1a6939f5f
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2019-12-23 16:03:33 +0100

    home screen part1

--- app/src/main/java/com/example/sharewishes/MainActivity.kt
+++ app/src/main/java/com/example/sharewishes/MainActivity.kt
@@ -1,28 +1,26 @@
 package com.example.sharewishes
 
 import android.os.Bundle
+import android.view.Menu
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        navView.setupWithNavController(navController)
+        setSupportActionBar(toolbar6)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home , menu)
+        return true
+    }
+
 }
