afd8dd36efdc37f796cc1b2f32b11b0e90d6e4e9
ansnoussi <snoussi.anis@insat.u-carthage.tn>
2019-12-23 20:12:50 +0100

    home screen part4

--- app/src/main/java/com/example/sharewishes/utils/Utils.kt
+++ app/src/main/java/com/example/sharewishes/utils/Utils.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.FragmentManager
 object Utils {
 
     fun openFragment(fragmentManager: FragmentManager , className: Class<out Fragment> , frameLayoutId : Int){
+        if(!isFragmentAlreadyAdded(fragmentManager, className)){
         val visibleFragment = visibleFragment(fragmentManager)
         val fragmentTransaction = fragmentManager.beginTransaction()
         if(visibleFragment != null){
@@ -13,18 +14,37 @@ object Utils {
         }
         val replacedFragment = className.newInstance()
         fragmentTransaction.add(frameLayoutId,replacedFragment,className::class.java.simpleName)
+        fragmentTransaction.commit()}
+        else{
+            showFragment(fragmentManager, className)
+        }
+    }
+
+    private fun showFragment(fragmentManager: FragmentManager, className: Class<out Fragment>) {
+        val fragmentList = fragmentManager.fragments
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        for(fragment in fragmentList){
+            if(fragment != null && fragment.javaClass.simpleName == className.simpleName){
+                fragmentTransaction.show(fragment)
+            }else{
+                fragmentTransaction.hide(fragment)
+            }
+        }
         fragmentTransaction.commit()
+    }
+
+    private fun isFragmentAlreadyAdded(
+        fragmentManager: FragmentManager,
+        className: Class<out Fragment>
+    ): Boolean {
+        val fragmentList = fragmentManager.fragments
+        return fragmentList.any { it != null && it.javaClass.simpleName == className.simpleName }
     }
 
     private fun visibleFragment(fragmentManager: FragmentManager): Fragment? {
         val fragmentList = fragmentManager.fragments
-        for(fragment in fragmentList){
-            if(fragment!= null && fragment.isVisible){
-                return fragment
-            }
-        }
 
-        return null
+        return fragmentList.firstOrNull { it != null && it.isVisible }
     }
 
 }
